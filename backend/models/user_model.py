79857b3ce0190074f7c52af69efac74903ad1066
himani <himanistha78@gmail.com>
2025-01-31 22:12:32 +0545

    Data update

--- backend/models/user_model.py
+++ backend/models/user_model.py
@@ -1,12 +1,21 @@
+"""
+This module defines the User model using Pydantic.
+"""
+
 from pydantic import BaseModel, EmailStr
 
 
 class User(BaseModel):
-    first_name: str
-    last_name: str
+    """
+    Represents a user with necessary fields for authentication.
+    """
+    
+    firstName: str
+    lastName: str
     email: EmailStr
     password: str
     confirmPassword: str
-    
+
     class Config:
+        """Configuration for the Pydantic model."""
         fields = {"confirmPassword": {"exclude": True}}
